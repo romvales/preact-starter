815c18d95a1f2dbd99f65e6f6bc7b5b9b449220e
Rom Rom <romdevmod@gmail.com>
2022-08-25 23:47:57 +0800

    Updated `App.test.tsx`

--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -3,12 +3,18 @@ import { App } from '../src/App'
 
 describe('App', () => {
 
+  beforeAll(() => {
+    window.clientRuntimeConfig = {
+      BASE_URL: 'http://localhost:3000'
+    }
+  })
+
   test('should contain a heading with a pangram message.', () => {
     history.replaceState({}, null, '/home')
     const { container } = render(<App />)
     const headline: HTMLHeadingElement = container.querySelector('.headline')
 
-    expect(headline.textContent).toBe('Smiling golden doggo')
+    expect(headline.textContent).toBe(`Smiling golden doggo ${clientRuntimeConfig.BASE_URL}`)
   })
 
 })
