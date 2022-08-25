3029e20af3244b9ce99afca63df6fcee6f86a008
Rom Rom <romdevmod@gmail.com>
2022-08-25 14:44:04 +0800

    Display doggo

--- src/views/LPageView.tsx
+++ src/views/LPageView.tsx
@@ -1,8 +1,11 @@
-
+import goldenRetrieverMp4 from '@/assets/images/hapi-golden-retriever.mp4'
 import { FunctionComponent } from 'preact'
 
 export const LPageView: FunctionComponent<{ path: string }> = (props) => {
   return (
-    <h1 className='headline'>The quick brown fox jumps over the lazy dog.</h1>
+    <>
+      <h1 className='headline'>Smiling golden doggo</h1>
+      <video src={goldenRetrieverMp4} autoPlay loop></video>
+    </>
   )
 }
