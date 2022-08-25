fe1755a8148cc379a618add4e159e01307f5e028
Rom Rom <romdevmod@gmail.com>
2022-08-25 23:51:44 +0800

    Did some cleanup on the SSR router.

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,17 +1,23 @@
+import '../../app.config'
+import { isDevelopment } from '@/helpers/ssr-utils'
 import { setupDevMiddleware, ssrRouter } from './middlewares'
 import morgan from 'morgan'
 import express from 'express'
 import cookieParser from 'cookie-parser'
 import path from 'path'
 
+// @ts-ignore
+import helmet from 'helmet'
+
 const server = express()
-const serverPort = parseInt(process.env.PORT) || 3000
 
 setupDevMiddleware(server)
 
-server.use(cookieParser())
-server.use(morgan('dev'))
+server.use(helmet(APP_CONFIG.helmetOptions))
+server.use(cookieParser('', APP_CONFIG.cookieParserOptions))
+server.use(morgan('dev', APP_CONFIG.morganOptions))
+
 server.use(express.static(path.resolve('dist'), { index: false }))
 server.use(ssrRouter)
 
-server.listen(serverPort, null, () => {})
+server.listen(APP_CONFIG.serverPort, APP_CONFIG.serverHost, () => {})
