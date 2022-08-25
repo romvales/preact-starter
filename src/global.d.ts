afdd316451533bbcc618b1a640e7cfe72d688227
Rom Rom <romdevmod@gmail.com>
2022-08-25 14:37:58 +0800

    Declared TS modules for resolving binary assets.

--- src/global.d.ts
+++ src/global.d.ts
@@ -6,3 +6,16 @@ interface Window {
   __UNISTORE_STATE__: { [key: string]: any },
   __APP_STATE__: { [key: string]: any },
 }
+
+declare module '*.mp4'
+declare module '*.3gp'
+declare module '*.png'
+declare module '*.ogg'
+declare module '*.mp3'
+declare module '*.m4a'
+declare module '*.jpg'
+declare module '*.jpeg'
+declare module '*.mkv'
+declare module '*.ico'
+declare module '*.flac'
+declare module '*.webp'
