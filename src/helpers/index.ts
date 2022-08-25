fb9b8feea1839e4e592caf06117d932ae378191d
Rom Rom <romdevmod@gmail.com>
2022-08-25 14:43:45 +0800

    Added `enableDevTools()` and other useful utilities.

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,5 +1,14 @@
 import { isDevelopment } from './ssr-utils'
 
+export { 
+  isDevelopment, 
+  isBrowser, 
+  isServer, 
+  isProduction,
+  initAppState,
+  initUniStore,
+} from './ssr-utils'
+
 export function enableDevTools() {
   if (isDevelopment) {
     require('preact/debug')
--- src/helpers/ssr-utils.ts
+++ src/helpers/ssr-utils.ts
@@ -1,4 +1,20 @@
+import createStore, { Store } from 'unistore'
+import devtools from 'unistore/devtools'
+
 export const isBrowser = typeof process === 'undefined' || typeof process.versions.node === 'undefined' || typeof Window === 'object'
 export const isServer = !isBrowser && typeof localStorage === 'undefined' && typeof self === 'undefined' && typeof process.version === 'string'
 export const isDevelopment = process.env.NODE_ENV === 'development'
 export const isProduction = !isDevelopment
+
+export function initUniStore(store?: Store<any>) {
+  const initState = window.__UNISTORE_STATE__ ?? {}
+  const C = createStore
+
+  return isDevelopment && isBrowser ? devtools(C(initState)) : C(initState)
+}
+
+export function initAppState() {
+  const initState = window.__APP_STATE__ ?? {}
+
+  return initState
+}
