c294fa20f53b43419392d82370328de1d8af4fea
Rom Rom <romdevmod@gmail.com>
2022-08-26 21:47:13 +0800

    Updated `tests/App.test.tsx`...

--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -1,20 +1,51 @@
 import { render } from '@testing-library/preact'
-import { App } from '../src/App'
+import { FunctionComponent } from 'preact'
+import { switchEnvironment } from './utils'
+import {
+  mockAppState,
+  mockDefaultProps,
+  mockPendingSSProps,
+
+  initializeGlobalMocksState,
+  teardownGlobalMocksState,
+
+} from './__mocks__/initializeMockState'
 
 describe('App', () => {
+  let App: FunctionComponent<any>
 
-  beforeAll(() => {
+  beforeEach(() => {
+    initializeGlobalMocksState()
     window.clientRuntimeConfig = {
       BASE_URL: 'http://localhost:3000'
     }
+
+    App = require('@/App').App
+  })
+
+  afterEach(() => {
+    teardownGlobalMocksState()
+  })
+
+  test('should define the necessary variable and objects...', () => {
+    expect(window).toBeDefined()
+    expect(window.__APP_STATE__).toBeDefined()
+    expect(pendingServerSideProps).toBeDefined()
+    expect(window.__APP_STATE__).toHaveProperty('serverSideProps')
+    expect(window.__APP_STATE__).toHaveProperty('serverSidePropsIndex')
   })
 
   test('should contain a heading with a pangram message.', () => {
+    initializeGlobalMocksState(App)
+    switchEnvironment()
+
     history.replaceState({}, null, '/home')
     const { container } = render(<App />)
-    const headline: HTMLHeadingElement = container.querySelector('.headline')
+    expect(container.querySelector('.headline').textContent).toBe(`Smilling golden doggo ${clientRuntimeConfig.BASE_URL}`)
+    expect(window.__UNISTORE_STATE__).toBeUndefined()
 
-    expect(headline.textContent).toBe(`Smiling golden doggo ${clientRuntimeConfig.BASE_URL}`)
+    teardownGlobalMocksState(App)
+    switchEnvironment()
   })
 
 })
