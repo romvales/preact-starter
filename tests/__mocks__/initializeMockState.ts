b60f72a1a21dd4bd14bd7e4944cf0304f9c12f05
Rom Rom <romdevmod@gmail.com>
2022-08-26 21:38:46 +0800

    Added globally capable test init functions.

---
+++ tests/__mocks__/initializeMockState.ts
@@ -0,0 +1,21 @@
+import { FunctionalComponent } from 'preact' 
+
+
+export let mockDefaultProps = {}
+export const mockPendingSSProps = []
+export const mockAppState = {
+  serverSideProps: [],
+  serverSidePropsIndex: 0,
+}
+
+export function initializeGlobalMocksState(TC?: FunctionalComponent<any>) {
+  if (TC) TC.defaultProps = mockDefaultProps = {}
+  window.__APP_STATE__ = mockAppState
+  global.pendingServerSideProps = mockPendingSSProps
+}
+
+export function teardownGlobalMocksState(TC?: FunctionalComponent<any>) {
+  if (TC) delete TC.defaultProps
+  window.__APP_STATE__ = undefined
+  global.pendingServerSideProps = undefined
+}
