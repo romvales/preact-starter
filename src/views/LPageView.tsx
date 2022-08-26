43f3aa5b7e802f1377031dd60285273b3af3e015
Rom Rom <romdevmod@gmail.com>
2022-08-26 21:45:59 +0800

    Updated `/src/views/*`

--- src/views/LPageView.tsx
+++ src/views/LPageView.tsx
@@ -1,12 +1,17 @@
 import { FunctionComponent } from 'preact'
+import { getServerSideProps } from '@/helpers'
 
 import dogVideo from '@/assets/images/hapi-golden-retriever.mp4'
 
-export const LPageView: FunctionComponent<{ path: string }> = (props) => { 
+export const LPageView: FunctionComponent<{ path: string }> = (props) => {
   return (
     <>
-      <h1 className='headline bg-gray-600 font-light'>Smiling golden doggo {clientRuntimeConfig.BASE_URL}</h1> 
-      <video src={dogVideo} autoPlay loop></video> 
+      <h1 className='headline bg-gray-300 font-light'>Smilling golden doggo {clientRuntimeConfig.BASE_URL}</h1>
+      <video src={dogVideo} autoPlay loop></video>
     </>
   )
 }
+
+getServerSideProps(LPageView, async() => {
+  return { data: 1 }
+})
