9ec30efb820b7639009b757dbf4a17c44c68d77a
Rom Rom <romdevmod@gmail.com>
2022-08-29 19:24:48 +0800

    Added `index.tsx`

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,24 @@
 import { hydrate, render } from 'preact'
 import { App } from '@/App'
 import { ServerContext } from '@/server/SSRContext'
-import { 
-  enableDevTools, 
-  initUniStore, 
-  initAppState, 
-  isDevelopment 
+import {
+  enableDevTools,
+  initUniStore,
+  initAppState,
+  isDevelopment
 } from '@/helpers'
 
 enableDevTools()
 
-function renderApp(App) {
+async function renderApp(App) {
   const appRootEl = document.querySelector('.app-root')
   const renderFunc = isDevelopment ? render : hydrate
 
   renderFunc(
     <ServerContext value={initAppState()} store={initUniStore()}>
       <App />
-    </ServerContext>  
-  , appRootEl)
+    </ServerContext>
+    , appRootEl)
 
   delete window.__UNISTORE_STATE__
 }
